1580ae70482fbc96a462be7ea00f8f4440e035be
Александр <bradwoord@gmail.com>
2023-04-04 17:54:32 +0500

    Добавьте файлы проекта.

---
+++ Tasks.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.CompilerServices;
+
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CodeWarsTask;
+
+using static Tasks;
+
+public class Tasks {
+    public static string Maskify(string masked) {
+        return masked.Length < 4 ? masked :
+            new string('#', masked[0..^4].Length) + masked.Substring(masked.Length-4);
+    }
+
+    public static Boolean IsIsogram(string str, Boolean? IgnoreCase = true) {
+        return str.ToLower().Distinct().Count() == str.Length;
+    }
+
+    public static string HighAndLow(string numbers) {
+        var arrNums = numbers.Split(' ').Select(n => Int32.Parse(n)).ToArray();
+        return arrNums.Max() + " " + arrNums.Min();
+    }
+
+    public static string Longest(string s1, string s2) {
+        return new string((s1 + s2).Distinct().OrderBy(c => c).ToArray());
+    }
+
+    public static string ToCamelCase(string str) {
+        var splitStrings = str.Split(new char[] { '-', '_', ' ' });
+        return splitStrings[0] + String.Join("", splitStrings.Skip(1).Select(s => Char.ToUpper(s[0]) + s[1..^0]));
+    }
+
+    public static string SpinWords(string sentence) {
+        return String.Join(" ", sentence.Split(' ').Select(x => x.Length < 5 ? x : new string(x.Reverse().ToArray())));
+    }
+
+    public static Boolean Narcissistic(int value) {
+        
+    }
+}
+
+internal class Start {
+    public static async Task Main() {
+        await Console.Out.WriteLineAsync(SpinWords("Hey fellow warriors"));
+    }
+}
+
+public class CleverSolution {
+    //a.kozhanov, использование регулярных выражений
+    public static string SpinWords(string sentence) {
+        return Regex.Replace(sentence, @"\w{5,}", m => string.Concat(m.Value.Reverse()));
+    }
+}
