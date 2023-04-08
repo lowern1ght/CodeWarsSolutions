09ed0217737bbb87826bf35924dcac7ba76a5f51
Alexander Sergeev <bradwoord@gmail.com>
2023-04-08 22:53:45 +0500

    and new solution camelcase

---
+++ 6 kyu/LongestConsecutives.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeWarsTask._6_kyu {
+    public class LongestConsecutives {
+        public static string LongestConsec(string[] strarr, int k) {
+            return (k > 0) && (k <= strarr.Length) ? String.Join("", strarr.Select((x, i) => x+strarr.ElementAtOrDefault(i)).OrderBy(s => s.Length).First()) : "";
+        }
+    }
+}
---
+++ 6 kyu/Problem.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWarsTask._6_kyu;
+
+public static class Problem {
+    public static string CamelCase(this string str) {
+        return Regex.Replace(str, @"\b[a-z]", m => m.Value.ToUpper()).Replace(" ", "");
+    }
+}
