64803b1c7339d256b1e10ba0ad44a02ed757c55e
Alexander Sergeev <bradwoord@gmail.com>
2023-04-09 00:02:44 +0500

    edit

---
+++ 6 kyu/StripCommentsSolution.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWarsTask._6_kyu;
+
+public class StripCommentsSolution {
+    public static string StripComments(string text, string[] commentSymbols) {
+        //return String.Join("", text.Split(@"\n").Select(s => Regex.Replace(s.ToString(), @"\s*["+String.Join("", commentSymbols)+"].+", m => "").Trim()));
+        return Regex.Replace(text.Replace("\n", Environment.NewLine), @"\s*["+String.Join("?", commentSymbols)+"].*$",
+            m => String.Empty, RegexOptions.Multiline);
+    }
+}
